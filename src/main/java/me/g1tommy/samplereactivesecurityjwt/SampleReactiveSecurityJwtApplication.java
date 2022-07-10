f0042e846547bf354ca6dcbaa03cb66254e64c4c
전지원(Jiwon Jeon)/Core플랫폼개발팀/11ST <jiwon_jeon@sk.com>
2022-07-10 15:52:29 +0900

    Initial Commit

---
+++ src/main/java/me/g1tommy/samplereactivesecurityjwt/SampleReactiveSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package me.g1tommy.samplereactivesecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleReactiveSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleReactiveSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/g1tommy/samplereactivesecurityjwt/SampleReactiveSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package me.g1tommy.samplereactivesecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleReactiveSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
